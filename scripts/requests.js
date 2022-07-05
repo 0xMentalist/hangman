746aabb0cf90ff70ebe004ee6932025d132f7bc3
0xMentalist <107797215+0xMentalist@users.noreply.github.com>
2022-07-05 18:49:36 +0530

    Update requests.js

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,10 +1,3 @@
-const startGame = async () => {
-    aWord = await fetchPuzzle(3).then((puzzle) => puzzle.puzzle).catch((err)=> console.log(err))
-    let firstGame = new Hangman(aWord)
-    return firstGame
-    }
-
-
 const fetchPuzzle = async (wordCount) => {
    const data = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)      
         if(data.status === 200) {
